b7cf0907e3000ce32a0669f9523601bdce650e3b
Ahmed Hashim <ahmedalaahashem01@gmail.com>
2022-01-19 14:53:23 +0200

    sort the imports

--- project/tests/test_views.py
+++ project/tests/test_views.py
@@ -1,10 +1,9 @@
-from pyexpat import model
-from django.urls import reverse
-from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
+from project import factories, models
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from project import factories, models
+from rest_framework.test import APITestCase
 
 
 class ProjectTest(APITestCase):
